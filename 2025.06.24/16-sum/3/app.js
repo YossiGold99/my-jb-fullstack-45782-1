35f02b94cbcb9a0ade05ac036640c321147654c0
Yossi <yyossi84@gmail.com>
2025-06-24 13:54:01 +0300

    16-sum-3

--- 2025.06.24/16-sum/3/app.js
+++ 2025.06.24/16-sum/3/app.js
@@ -1,9 +1,13 @@
 let input = +prompt('please enter a number')
 let sum = 0;
+let count = 0;
 
 while (input != 0) {
     sum += input
+    // count +=1
+    count++
     input = +prompt('please enter a number')
 }
 
-alert(`sum is ${sum}`)
+let avg= sum/count;
+alert(`${count === 0 ? : 'can not calculate without numbers' : sum/count }`)
