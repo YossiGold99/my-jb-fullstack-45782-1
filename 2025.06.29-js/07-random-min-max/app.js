76f0b07998ab6a9b6d4f1b7b3b58e0b8e07f236c
Yossi <yyossi84@gmail.com>
2025-06-29 11:35:51 +0300

    random-min-max

---
+++ 2025.06.29-js/07-random-min-max/app.js
@@ -0,0 +1,8 @@
+const min =+prompt("enter a min")
+const max =+prompt("enter a max")
+
+
+const random = Math.random(); //0 - 0.9999
+console.log(parseInt(random * (max - min +1)) + min);; 
+// console.log(parseInt(random * 20));
+// console.log(parseInt(random * 20) + 10); 
