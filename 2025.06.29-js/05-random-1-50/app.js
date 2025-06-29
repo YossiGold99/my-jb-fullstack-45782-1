4c653031cc7ebd846fa1fff5f5577590f88293b9
Yossi <yyossi84@gmail.com>
2025-06-29 11:27:13 +0300

    10-30

--- 2025.06.29-js/05-random-1-50/app.js
+++ 2025.06.29-js/05-random-1-50/app.js
@@ -1,4 +1,4 @@
-const random = Math.random(); //0 - 0.9999
-console.log(random * 50); // 0 - 99
-console.log(parseInt(random * 50)); // 0 - 99
-console.log(parseInt(random * 50) + 1);  //  1- 100
+const random = Math.random(); 
+console.log(random * 50);
+console.log(parseInt(random * 50)); 
+console.log(parseInt(random * 50) + 1); 
