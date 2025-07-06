21d3c72afc8dc16b64e574fb5bf9f7d5c02f6c1d
Yossi <yyossi84@gmail.com>
2025-07-06 11:10:30 +0300

    Arrangement

--- 2025.07.06/05-functions-print-styled-name/app.js
+++ 2025.07.06/05-functions-print-styled-name/app.js
@@ -5,13 +5,13 @@ function printStyledledName (anyname){
     console.log(`${anyname[0].toUpperCase}${anyname.substring(1)}`);
 }
 
-function printStyledFirstName() {
-    console.log(`${firstName[0].toUpperCase()} ${firstName.substring}`);
-}
+// function printStyledFirstName() {
+//     console.log(`${firstName[0].toUpperCase()} ${firstName.substring}`);
+// }
 
-function printStyledlastName() {
-    console.log(`${lastNane[0].toUpperCase()} ${lastNaneName.substring}`);
-}
+// function printStyledlastName() {
+//     console.log(`${lastNane[0].toUpperCase()} ${lastNaneName.substring}`);
+// }
 
 // printStyledFirstName
 printStyledName(firstName);
