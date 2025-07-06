988f725b6e03abe735a33d12158574180666b258
Yossi <yyossi84@gmail.com>
2025-07-06 10:48:49 +0300

    homework

--- 2025.07.06/1-homework-solution-03-04/app.js
+++ 2025.07.06/1-homework-solution-03-04/app.js
@@ -1,24 +1,9 @@
-const password = prompt(`enter password`)
+const myCat = {
+  name: "kitty",
+  age: 2,
+  color: "ginger",
+  isCastrated: true,
+};
 
-let isSixCharsLong = password.length >= 6;
-let hasCapitalLetter = password.toLowerCase() !== password;
-let haLowerCaseLetter = password.toUpperCase() !== password;
-let hasDigit = false;
-let hasSpecialChar = false;
-
-for (const char of password) {
-    if (`1234567890`.includes(chae)) hasDigit = true
-    if (Number(char) !== NaN) hasDigit = true
-}
-
-
-if (hasCapitalLetter && hasLowerCaseLetter && hasDigit && hasSpecialChar && isSixCharsLong) {
-    console.log(`great password`)
-} else {
-    //   if (isSixCharsLong === false) console.log("too short");
-    if (!isSixCharsLong) console.log("too short");
-    if (hasCapitalLetter === false) console.log("need capital letter");
-    if (hasLowerCaseLetter === false) console.log("need lower case letter");
-    if (hasDigit === false) console.log("need digit");
-    if (hasSpecialChar === false) console.log("need special char");
-}
+const catJSON = JSON.stringify(myCat);
+alert(catJSON);
