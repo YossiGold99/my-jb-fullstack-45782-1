44a1ee9386378702d5540e84fa25ce4e30b77284
Yossi <yyossi84@gmail.com>
2025-07-06 11:08:33 +0300

    funcation

---
+++ 2025.07.06/04-functions/app.js
@@ -0,0 +1,10 @@
+function printSpearatorLine (){
+    document.write("*******");
+    document.write("<br>");
+}
+
+printSpearatorLine();
+printSpearatorLine();
+printSpearatorLine();
+
+for (let i = 1; 1 <= 10; i++) printSpearatorLine();
---
+++ 2025.07.06/05-Dry-dont-repat-yourself/app.js
@@ -0,0 +1,13 @@
+const firstName = prompt("enter firsr name");
+const lastNane = prompt("enter last naem");
+
+function printStyledFirstName() {
+    console.log(`${firstName[0].toUpperCase()} ${firstName.substring}`);
+}
+
+function printStyledlastName() {
+    console.log(`${lastNane[0].toUpperCase()} ${lastNaneName.substring}`);
+}
+
+printStyledFirstName();
+printStyledlastName();
