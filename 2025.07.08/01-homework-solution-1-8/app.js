89f746c6800b31d1aa6dbab28e4230fb162e5acd
Yossi <yyossi84@gmail.com>
2025-07-08 10:19:53 +0300

    DOM

--- 2025.07.08/01-homework-solution-1-8/app.js
+++ 2025.07.08/01-homework-solution-1-8/app.js
@@ -1,16 +1,23 @@
-function showCurrentTime() {
+// function showCurrentTime() {
+//   const now = new Date();
+//   const hours = now.getHours();
+//   const minutes = now.getMinutes();
+//   const seconds = now.getSeconds();
+
+//   const timeString = hours.toString().padStart(2, '0') + ":" +
+//                      minutes.toString().padStart(2, '0') + ":" +
+//                      seconds.toString().padStart(2, '0');
+
+//   console.log("השעה הנוכחית היא: " + timeString);
+// }
+
+// // קריאה לפונקציה
+// showCurrentTime();
+
+
+function getCurrentTime() {
   const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  const timeString = hours.toString().padStart(2, '0') + ":" +
-                     minutes.toString().padStart(2, '0') + ":" +
-                     seconds.toString().padStart(2, '0');
-
-  console.log("השעה הנוכחית היא: " + timeString);
+  return now.toLocaleTimeString();
 }
 
-// קריאה לפונקציה
-showCurrentTime();
-
+console.log(`current time is ${getCurrentTime()}`);
