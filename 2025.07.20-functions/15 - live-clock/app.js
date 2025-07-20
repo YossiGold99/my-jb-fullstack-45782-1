0152ef1984fcf42f131afc5bb466aac7f83fa996
Yossi <yyossi84@gmail.com>
2025-07-20 15:23:51 +0300

    clock

---
+++ 2025.07.20-functions/15 - live-clock/app.js
@@ -0,0 +1,8 @@
+const displayTime = () => {
+    const now = (new Date()).toLocaleTimeString()
+    document.getElementById('clock').innerHTML = now
+}
+displayTime()
+setInterval(displayTime, 1 * 1000)
+
+
