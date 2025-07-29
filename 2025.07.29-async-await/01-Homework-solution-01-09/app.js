a318e1bf4416e30da51541ce69b323bfe6bf8233
Yossi <yyossi84@gmail.com>
2025-07-29 11:27:07 +0300

    alota

---
+++ 2025.07.29-async-await/01-Homework-solution-01-09/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(() => {
+    const getRandomNumberAfterDelay  = limit => {
+        setTimeout(() => {}, 5000)
+        const random = Math.random() * limit
+        useCallback(random)
+    }
+
+    document.getElementById('random-button')
+})
+
