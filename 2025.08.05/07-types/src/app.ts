cd4420fc10c8d87202ca659b589f43fd7fa4df7e
Yossi <yyossi84@gmail.com>
2025-08-17 09:52:31 +0300

    DRY demo

---
+++ 2025.08.05/07-types/src/app.ts
@@ -0,0 +1,31 @@
+// primitives
+
+const age: number = 23
+
+const message: string = 'hello world'
+
+const isActive: boolean = true
+
+// arrays
+
+const ages: number[] = [23, 42, 66]
+
+const messages: string[] = ['hello', 'world']
+
+const someFlags: boolean[] = [true, false, true]
+
+const mixedArray: unknown[] = [1, 'hello', true]
+
+
+// object
+const myObj: object = {}
+
+type User ={ name: string, age: number, birthdate: Date }
+
+const aUser:User  = {
+    name: 'uval',
+    age: 22,
+    birthdate: new Date(),
+}
+
+const Users: User[] = []
