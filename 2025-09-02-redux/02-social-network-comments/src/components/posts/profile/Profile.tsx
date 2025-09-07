2828ea7cf926224b4a73ade1595fef792bd3fc42
Yossi <yyossi84@gmail.com>
2025-09-07 13:19:55 +0300

    profile slice wip

---
+++ 2025-09-02-redux/02-social-network-comments/src/components/posts/profile/Profile.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import './Profile.css'
+import profileService from '../../../services/profile'
+import type PostModel from '../../../models/post'
+import Post from '../post/Post'
+import NewPost from '../new/NewPost'
+import type PostComment from '../../../models/post-comment'
+
+export default function Profile() {
+
+    console.log('rendering...')
+
+    const [profile, setProfile] = useState<PostModel[]>([])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const profileFromServer = await profileService.getProfile()
+                setProfile(profileFromServer)
+            } catch (e) {
+                alert(e)
+            }
+        })()
+    }, [])
+
+    function removePost(id: string): void {
+        setProfile(profile.filter(post => post.id !== id))
+    }
+
+    function renderNewPost(post: PostModel): void {
+        setProfile([post, ...profile])
+    }
+
+    function newComment(comment: PostComment) {
+        const newProfile = [...profile]
+        const post = newProfile.find(post => post.id === comment.postId)
+        post?.comments.push(comment)
+        // setProfile(profile)
+        setProfile(newProfile)
+    }
+
+    return (
+        <div className='Profile'>
+            <NewPost
+                renderNewPost={renderNewPost}
+            />
+            {profile.map(post => <Post
+                key={post.id}
+                post={post}
+                removePost={removePost}
+                isEditAllowed={true}
+                newComment={newComment}
+            />)}
+        </div>
+    )
+}
