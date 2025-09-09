85e5ad114ca9d1d325a904f9cd270b2e69d9871f
Yossi <yyossi84@gmail.com>
2025-09-09 14:55:52 +0300

    sockets

---
+++ 2025-09-09-sockets/03-social-network-sockets/src/services/auth-aware/feed.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+import type Post from "../models/post"
+
+class FeedService {
+    async getFeed(): Promise<Post[]> {
+        const { data } = await axios<Post[]>(`${import.meta.env.VITE_REST_SERVER_URL}/feed`)
+        return data
+    }
+
+
+}
