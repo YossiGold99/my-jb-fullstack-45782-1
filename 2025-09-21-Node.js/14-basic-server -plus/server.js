5a7ef3de11a6b24b0c9dcd0ad8ea2ad57b9c5c80
Yossi <yyossi84@gmail.com>
2025-09-21 15:52:49 +0300

    basic server plus

--- 2025-09-21-Node.js/14-basic-server -plus/server.js
+++ 2025-09-21-Node.js/14-basic-server -plus/server.js
@@ -1,1 +1,6 @@
-require('http').createServer(() => { ((request, Response) => Response.end('hello world')).listen(3000) })
+const { createServer } = require('http')
+const PORT = process.env.PORT || 3000
+
+const requestHandler = (request, Response) => Response.end('hello world')
+
+createServer().listen(PORT, () => console.log('server started....'))
